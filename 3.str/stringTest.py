ef882d2c13bc5659aab8ee2ce91f0238c8015e3c
kcshan <cshan.kong@gmail.com>
2020-10-01 12:28:50 +0800

    3.str

---
+++ 3.str/stringTest.py
@@ -0,0 +1,29 @@
+# stringTest = "zygx8 lry"
+# string_test = 'zygx8 lry'
+# print(stringTest, string_test)
+print('zygx8 \n lry')
+print('\tzygx8')
+print('\\')
+
+print('\'')
+print(' " ')
+print(" ' ")
+
+input_test = input('>>>')
+print('您输入：', input_test)
+
+name = 'lry'
+print('your name is %s' % name)
+
+intTest = 123
+print('you int is %d' % intTest)
+
+rformat = '"551:"'
+print('your input is %r' % rformat)
+
+print(1 == 2)
+
+from sys import argv
+
+print('你输入的参数是：%r' % argv[1])
+
